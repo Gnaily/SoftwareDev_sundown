adb0474e71b0301c87cb728584f0b0db97ca991a
Brock Fenbert <brock@Brocks-MacBook-Pro.local>
2020-09-13 19:08:59 -0400

    finish main and create jar

--- B/Other/src/Xyes.java
+++ B/Other/src/Xyes.java
@@ -1,13 +1,11 @@
 /**
- *
+ * Class for Running the Xyes printing programs
  */
 public class Xyes {
 
   public static void main(String[] args) {
-    String wordToPrint = "hello world";
-    boolean limit = false;
-
-
+    boolean limit = args.length > 0 && args[0].equals("-limit");
+    String wordToPrint = getWordToPrint(args);
 
     int count = 0;
 
@@ -18,10 +16,12 @@ public class Xyes {
 
   }
 
-  public static boolean shouldLimit(String limit) {
-    return limit.equals("-limit");
-  }
-
+  /**
+   * Gets the word to print out to the command line
+   *
+   * @param args Command line arguments passed in to the function
+   * @return The word to print
+   */
   public static String getWordToPrint(String[] args) {
     String word = "";
     for (int i = 0; i < args.length; i++) {
--- B/Other/test/XyesTest.java
+++ B/Other/test/XyesTest.java
@@ -1,16 +1,8 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class XyesTest {
-
-  @Test
-  public void shouldLimit() {
-    assertTrue(Xyes.shouldLimit("-limit"));
-    assertFalse(Xyes.shouldLimit("hello"));
-  }
 
   @Test
   public void testGetWord() {
