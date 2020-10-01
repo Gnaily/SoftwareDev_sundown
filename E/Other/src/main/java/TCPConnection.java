6e628f3e54360ecdf567cd0d741d9e53129e4528
Alanna Pasco <pasco.a@husky.neu.edu>
2020-10-01 14:35:30 -0400

    adjusted methods to process command line arguments separately

--- E/Other/src/main/java/TCPConnection.java
+++ E/Other/src/main/java/TCPConnection.java
@@ -9,30 +9,14 @@ import java.util.Scanner;
  * Class for handling a TCP connection and returning JSON
  */
 public class TCPConnection {
+  public static final int TIMEOUT = 3000;
+  public static final int DEFAULTPORT = 4567;
 
   public static void main(String[] args) throws IOException {
-
-    // handle determining the port to open
-    int port = 4567;
-    if (args.length > 1) {
-      // passed in more than one command line argument
-      System.out.println("usage: ./xtcp positive-integer");
-      return;
-    }
-    if (args.length == 1) {
-      try {
-        port = Integer.parseInt(args[0]);
-      }
-      catch (Exception e) {
-        // number passed in was not an integer
-        System.out.println("usage: ./xtcp positive-integer");
-        return;
-      }
-    }
-
+    int port = processCliArgs(args);
     // open network for incoming message
     ServerSocket serverSocket = new ServerSocket(port);
-    serverSocket.setSoTimeout(3000);
+    serverSocket.setSoTimeout(TIMEOUT);
     Socket socket = serverSocket.accept();
     InputStream inputStream = socket.getInputStream();
     Scanner scan = new Scanner(inputStream);
@@ -42,11 +26,34 @@ public class TCPConnection {
     json.processInput(scan);
     String output = json.formatJson();
 
-
     // send message back & end program
     OutputStream outputStream = socket.getOutputStream();
     outputStream.write(output.getBytes());
     serverSocket.close();
     socket.close();
+  }
+
+  /**
+   * Processes the command line arguments. If no arguments are given, use default port number.
+   * @param args command line arguments containing the port number or empty to use default
+   * @return the port number to connect to
+   */
+  public static int processCliArgs(String[] args) {
+    int port = DEFAULTPORT;
+    if (args.length > 1) {
+      System.out.println("usage: ./xtcp positive-integer");
+      System.exit(1);
+    }
+    if (args.length == 1) {
+      try {
+        port = Integer.parseInt(args[0]);
+      }
+      catch (Exception e) {
+        // number passed in was not an integer
+        System.out.println("usage: ./xtcp positive-integer");
+        System.exit(1);
+      }
+    }
+    return port;
   }
 }
