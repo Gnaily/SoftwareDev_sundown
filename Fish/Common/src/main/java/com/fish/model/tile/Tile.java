6337c38dfb0995c0b8e39ed6446bbb5a91b08df5
Alanna Pasco <pasco.a@husky.neu.edu>
2020-10-10 11:15:30 -0400

    wrote comments for all methods in the GameBoard class and interface

--- Fish/Common/src/main/java/com/fish/model/tile/Tile.java
+++ Fish/Common/src/main/java/com/fish/model/tile/Tile.java
@@ -1,9 +1,10 @@
 package com.fish.model.tile;
 
 /**
- * Interface for a Tile object in a game of Hey, That's my Fish!
- * An implementation of the Tile object's only functionality is to return the number of fish
- * on the tile, which is used to calculate the score of a game.
+ * Interface for a Tile object in a game of Hey, That's my Fish! (HTMF)
+ * An implementation of the Tile object contains only an integer representing the number of fish
+ * on the tile, and its only functionality is to return that number of fish.
+ * This information is used to calculate the score of a game of HTMF.
  */
 public interface Tile {
 
